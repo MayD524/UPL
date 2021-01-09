d048eba732e43199d8541b0b2603b502a05b3d33
Ryan <57610348+RyanD524@users.noreply.github.com>
2021-01-08 19:14:33 -0500

    Add files via upload

--- UPL/gui_impliments.py
+++ UPL/gui_impliments.py
@@ -4,7 +4,6 @@ from win10toast import ToastNotifier
 from UPL.Core import file_manager as fm
 from UPL import Core as cr
 import pyautogui
-import calendar
 import os
 
 LARGE_FONT= ("Verdana", 12)
@@ -111,39 +110,3 @@ def send_notification(icon=None,threaded=True, title=None, message=None):
 			message,
 			threaded=threaded
 			)
-
-
-class TkinterCalendar(calendar.Calendar):
-	def select_json(self, json_file):
-		self.json = json_file
-
-	def check_events(self, date, button_name):
-		data = fm.getData_json(self.json)
-		if date in data.keys():
-			popup(title="Calendar", msg = f"On {date}, you have {data[date]}")
-		else:
-			popup(title="Calendar", msg = f"On {date} you have no events.")	
-	def formatmonth(self, master, year, month):
-
-		dates = self.monthdatescalendar(year, month)
-
-		frame = tk.Frame(master)
-
-		self.labels = []
-		self.button_ids = {}
-		for r, week in enumerate(dates):
-			labels_row = []
-			for c, date in enumerate(week):
-				label = tk.Button(frame, text=date.strftime('%Y\n%m\n%d'), command=lambda: self.check_events(date.strftime('%Y-%m-%d'), text))
-				label.grid(row=r, column=c)
-
-				if date.month != month:
-					label['bg'] = '#aaa'
-
-				if c == 6:
-					label['fg'] = 'red'
-
-				labels_row.append(label)
-				self.button_ids[label] = date.strftime("%Y-%m-%d")
-			self.labels.append(labels_row)
-		return frame
